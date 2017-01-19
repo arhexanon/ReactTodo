d1613b939dd636e6a304e3606d9ca2838d57a53a
vanzijp <vanzijp@gmail.com>
2017-01-19 11:16:15 +0100

    date created, date completed, tests

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,11 +1,18 @@
 var React =  require('react');
+var moment = require('moment');
 
 var Todo = React.createClass({
     render: function () {
 
         // Received props from  Todolist...
         // Using ES6 destructering...
-        var {id, text, completed} = this.props;
+        var {id, text, completed, createdAt, completedAt} = this.props;
+
+        var messageCompleted = "";
+        if (completed){
+            var messageCompleted = ", completed at: " + moment.unix(completedAt).format('MMMM Do, YYYY @ h:mm A');
+        }
+
 
         // react expression syntax uses {}...
         return (
@@ -13,7 +20,7 @@ var Todo = React.createClass({
                 this.props.onToggle(id);
             }}>
                 <input type="checkbox" checked={completed}/>
-                {text} {id}
+                {text},  {id}, created at: {moment.unix(createdAt).format('MMMM Do, YYYY @ h:mm A')} {messageCompleted}
             </div>
         )
     }
