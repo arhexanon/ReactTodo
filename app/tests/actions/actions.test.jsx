4a082ef4967e7259dae73392ae0d64b24fd748a6
vanzijp <vanzijp@gmail.com>
2017-01-20 15:10:09 +0100

    actions & test added

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,33 @@
+var expect  = require('expect');
+var actions = require('actions');
+
+
+describe('Actions', () => {
+    it('should generate search text action', () => {
+        var action = {
+            type: 'SET_SEARCH_TEXT',
+            searchText: 'Some text to search'
+        }
+        expect(actions.setSearchText(action.searchText)).toEqual(action);
+    });
+    it('should generate add todo action', () => {
+        var action = {
+            type: 'ADD_TODO',
+            text: 'Walk the dog'
+        }
+        expect(actions.addTodo("Walk the dog")).toEqual(action);
+    });
+    it('should generate toggle show completed action', () => {
+        var action = {
+            type: 'TOGGLE_SHOW_COMPLETED'
+        }
+        expect(actions.toggleShowCompleted()).toEqual(action);
+    });
+    it('should generate toggle todo action', () => {
+       var action = {
+           type: 'TOGGLE_TODO',
+           id: 1
+       }
+       expect(actions.toggleTodo(action.id)).toEqual(action);
+    });
+});
--- app/tests/components/TodoSearch.test.jsx
+++ app/tests/components/TodoSearch.test.jsx
@@ -19,7 +19,7 @@ describe('TodoSearch', () => {
             // The function TodoSearch.handleSearchTodo is simulated with the spy...
             var spy =  expect.createSpy();
 
-            var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearchTodo={spy}/>);
+            var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
             todoSearch.refs.showCompleted.valuemail = false;
             todoSearch.refs.searchText.value = "Check mail";
 
@@ -30,7 +30,7 @@ describe('TodoSearch', () => {
         it('should call onTodoSearch if checkbox is set', () => {
             var spy =  expect.createSpy();
 
-            var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearchTodo={spy}/>);
+            var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
             todoSearch.refs.showCompleted.checked = true;
             todoSearch.refs.searchText.value = "Check mail";
 
